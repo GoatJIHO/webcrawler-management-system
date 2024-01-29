e70dc88cb0640a0e055d0e604e82b357c23274c7
GoatJIHO <wlgh6709@kumoh.ac.kr>
2024-01-29 15:24:28 +0900

    feat: admin 작성

--- django/crawler_application/admin.py
+++ django/crawler_application/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Site
 
 # Register your models here.
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+  list_display = ('site_name', 'last_update')
+  search_display = ('site_name', 'last_update')
