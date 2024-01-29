3d0157d19f14e8a46c538b05eb4047ac4743bf24
GoatJIHO <wlgh6709@kumoh.ac.kr>
2024-01-29 15:25:09 +0900

    feat: 모델구조 정의

--- django/crawler_application/models.py
+++ django/crawler_application/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+  site_name = models.CharField(max_length=128, null=True, default=None)
+  last_update = models.DateTimeField(null=True)
+
+  def __str__(self):
+    return self.site_name
